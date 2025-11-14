73dbc25ef4881658de5764c42a8b28d437983e7b
Renato Carvalho de Souza <renato.souza@economia.gov.br>
2025-11-14 15:12:39 -0300

    Ajustes

--- header.php
+++ header.php
@@ -13,6 +13,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <?php wp_head(); ?>
+    <script type="application/ld+json">
+      {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "Bebelume",
+        "legalName": "Bebelume",
+        "url": "https://bebelume.com.br",
+        "logo": "https://bebelume.com.br/wp-content/uploads/2025/11/cropped-logo-bebelume-play.png",
+        "description": "Portal Bebelume: filmes, séries, jogos e arte para a primeira infância. Conteúdos criativos para famílias, escolas e educadores.",
+        "sameAs": [
+        "https://www.instagram.com/bebelume/",
+        "https://www.youtube.com/c/bebelume",
+        "https://www.facebook.com/BEBELUME/"
+        ]
+      }
+		</script>
+
 </head>
 
 <body <?php body_class(); ?>>
